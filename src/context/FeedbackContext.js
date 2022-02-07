9f67b4d2bff6b9d6e055463fa33ff974b5bad80d
OkkashaAlly <steamnature006@gmail.com>
2022-02-07 12:14:43 +0300

    Connecting server with the app

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,33 +1,42 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    { id: 1, text: "This is feedback item 1", rating: 10 },
-    { id: 2, text: "This is feedback item 2", rating: 7 },
-    { id: 3, text: "This is feedback item 3", rating: 8 },
-  ]);
-
+  const [feedback, setFeedback] = useState([]);
   const [feedbackEdit, setFeedbackEdit] = useState({ item: {}, edit: false });
 
+  useEffect(async _ => {
+    const response = await fetch(
+      "http://localhost:5000/feedback?_sort=id&_order=desc"
+    );
+
+    const data = await response.json();
+
+    setFeedback(data);
+  }, []);
+
+  // Delete feedback
   const deleteFeedback = id => {
     if (window.confirm("Are you sure you want to delete this !?")) {
       setFeedback(feedback.filter(item => item.id !== id));
     }
   };
 
+  // Add feedback
   const addFeedback = newFeedback => {
     newFeedback.id = uuidv4();
 
     setFeedback([newFeedback, ...feedback]);
   };
 
+  // Set feedback to edit
   const editFeedback = item => {
     setFeedbackEdit({ item, edit: true });
   };
 
+  // Update feedback
   const updateFeedback = (id, updatedItem) => {
     setFeedback(
       feedback.map(item => {
