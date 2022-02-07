7511dc19c4c928e02e7845e09d2121d836fd6051
OkkashaAlly <steamnature006@gmail.com>
2022-02-07 10:55:56 +0300

    FeedbackStats - fixed average decimal points

--- src/componets/FeedBackStats.jsx
+++ src/componets/FeedBackStats.jsx
@@ -7,6 +7,8 @@ function FeedBackStats() {
   let average =
     feedback.reduce((acc, curr) => acc + curr.rating, 0) / feedback.length;
 
+  average = average.toFixed(1).replace(/[.,]0$/, "");
+
   return (
     <div className="feedback-stats">
       <h4>{feedback.length} Reviews</h4>
